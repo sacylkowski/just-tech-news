74c0b5ab061d40ad43ffd635691c89708546a10a
sacylkowski <sacylkowski@gmail.com>
2022-05-02 21:56:26 -0500

    homepage done

---
+++ controllers/home-routes.js
@@ -0,0 +1,61 @@
+// will contain all of the user-facing routes
+
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+// router.get("/", (req, res) => {
+//     // can accept an object as a second arguement
+//     // this object mimics what we will ultimately get from Sequelize
+//     res.render("homepage", {
+//         // every property of the object becomes available in the template using Handlebars.
+//         id: 1,
+//         post_url: "https://handlebarsjs.com/guide/",
+//         title: 'Handlebars Docs',
+//         created_at: new Date(),
+//         vote_count: 10,
+//         comments: [{}, {}],
+//         user: {
+//             username: 'test_user'
+//         }
+//     });
+// });
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        // this will loop over and map each Sequelize object into a serialized version of itself
+        // saving the results in a new posts array instead of an object
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
+const path = require("path");
 const express = require('express');
-const routes = require('./routes');
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const routes = require('./controllers/');
 // where our connection is
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
 
+
 // turn on connection to db and server
 // force: if set to true it would drop and re-create all of the database tables on startup
 // works like the DROP TABLE IF EXISTS
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
