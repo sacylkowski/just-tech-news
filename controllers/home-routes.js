b37cd3b00b0e32d704f7af5da98808eabed0f398
sacylkowski <sacylkowski@gmail.com>
2022-05-03 17:05:39 -0500

    single posts working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,51 +22,118 @@ const { Post, User, Comment } = require("../models");
 // });
 
 router.get('/', (req, res) => {
-    console.log(req.session);
+  console.log(req.session);
 
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
           model: User,
           attributes: ['username']
         }
-      ]
-    })
-      .then(dbPostData => {
-        // pass a single post object into the homepage template
-        // this will loop over and map each Sequelize object into a serialized version of itself
-        // saving the results in a new posts array instead of an object
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-  // the login page doesn't need any variables, so we don't need to pass a second argument to the render() method
-  router.get("/login", (req, res) => {
-      if (req.session.loggedIn) {
-          res.redirect("/");
-          return;
+      },
+      {
+        model: User,
+        attributes: ['username']
       }
-      res.render("login");
-  });
+    ]
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      // this will loop over and map each Sequelize object into a serialized version of itself
+      // saving the results in a new posts array instead of an object
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// the login page doesn't need any variables, so we don't need to pass a second argument to the render() method
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+// router.get("/post/:id", (req, res) => {
+//   const post = {
+//     id: 1,
+//     post_url: "https://handlebarsjs.com/guide/",
+//     title: "Handlebars Docs",
+//     created_at: new Date(),
+//     vote_count: 10,
+//     comments: [{}, {}],
+//     user: {
+//       username: "test-user"
+//     }
+//   };
+
+//   res.render("single-post", { post });
+// });
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [sequelize.literal("(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"), "vote_count"]
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
